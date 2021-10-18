f0f29a94d3bcb256ce0e8a5126240eb70fe02251
Collin <none>
2021-10-18 03:49:30 -0700

    remove folder

--- api/routes/index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-//const ctrlLocations = require('../controllers/locations');
-//const ctrlReviews = require('../controllers/reviews');
-
-module.exports = router;
